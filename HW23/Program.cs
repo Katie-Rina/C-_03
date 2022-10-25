b0a02a608bf28955f2d422f71f54fe74c5fe4598
KatieRina <eashchukina@yandex.ru>
2022-10-25 23:07:49 +0300

    третья задача

--- HW23/Program.cs
+++ HW23/Program.cs
@@ -1,4 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
-
+Console.Clear();
+Console.Write("Введите натуральное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N <= 0 )
+{
+    Console.WriteLine("Число не натуральное");
+}
+else
+{
+    for (int i = 1; i < N; i++)
+    {
+        Console.Write($"{Math.Pow(i,3)}, ");
+    }
+    Console.WriteLine($"{Math.Pow(N,3)}.");
+}
 
