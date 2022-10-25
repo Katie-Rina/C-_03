dc593a778a586b5ae5c89bbff805ad9736df8e50
KatieRina <eashchukina@yandex.ru>
2022-10-26 00:31:44 +0300

    первая задача

---
+++ HW19/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+bool PalindromeYesNo (int number)
+{
+    bool pal = false;
+    if ((number / 10000 == number % 10) && (number / 1000 % 10 == number % 100 / 10))
+    {
+        pal = true;
+    }
+    return pal;
+}
+Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 10000 | number > 99999)
+{
+    Console.WriteLine("Число не пятизначное");
+}
+else
+{
+    bool p = PalindromeYesNo(number);
+    if (p == true) Console.WriteLine("Палиндром");
+    else Console.WriteLine("Не палиндром");
+}
